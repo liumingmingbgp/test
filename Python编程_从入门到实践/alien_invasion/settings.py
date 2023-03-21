01eb92c4853fb254ca92f0c55445d2c8bb5d6b62
Liumingming <liumingmingbgp@163.com>
2023-03-21 18:48:45 +0800

    master

---
+++ Python编程_从入门到实践/alien_invasion/settings.py
@@ -0,0 +1,7 @@
+class Seetings:
+    '''存储游戏中所有的设置的类'''
+    def __init__(self):
+        '''初始化游戏的设置'''
+        self.screen_width = 900
+        self.screen_heigh = 600
+        self.bg_color = (230, 230,230)
